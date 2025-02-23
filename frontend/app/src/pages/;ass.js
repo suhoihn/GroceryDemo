ba4a4351940f1518bd8476aa35fc8c0b2193f354
modic <rain4therest@gmail.com>
2025-02-23 09:20:16 +0000

    Frontend changes

---
+++ frontend/app/src/pages/;ass.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import giveSomeYummyStuff from '../api.js';
+
+function Mainpage() {
+    const [colorIdx, setColorIdx] = useState(0);
+    const [msg, setMsg] = useState('');
+
+    const [name, setName] = useState("");
+    const [quantity, setQuantity] = useState(0);
+
+    const colors = ['red', 'blue', 'green', 'orange', 'purple'];
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setColorIdx((colorIdx + 1) % colors.length);
+        }, 10);
+
+        return () => clearInterval(intervalId);
+    });
+
+    return (
+        <div className='App'>
+            <h1 style={{color: colors[colorIdx]}}>
+                Welcome to the Main Page!
+            </h1>
+            <button onClick={() => { giveSomeYummyStuff(name, quantity) }}>
+                Click me to talk to backend chan!
+            </button>
+            <input value={name} onChange={(e) => setName(e.target.value)} />
+            <input value={quantity} onChange={(e) => setQuantity(e.target.value)} />
+            <p>
+                Hi my name is {msg}
+            </p>
+        </div>
+    )
+}
+
+export default Mainpage;
